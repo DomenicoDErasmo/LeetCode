f3e455f2af23d0848e728ab1ab720ee7fac8eceb
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2022-08-31 20:54:17 -0400

    LeetCode #136: Single Number

---
+++ SingleNumber/single_number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <set>
+
+class Solution {
+public:
+    int singleNumber(std::vector<int>& nums) {
+        std::set<int> uniques;
+        for (size_t i = 0; i < nums.size(); i++) {
+            if (!uniques.count(nums[i])) {
+                uniques.insert(nums[i]);
+            } else {
+                uniques.erase(nums[i]);
+            }
+        }
+        return *uniques.begin();
+    }
+};
+
+int main() {
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
