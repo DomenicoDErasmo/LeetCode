33c73b7346bc6e16c76061c781875f9dee122aa4
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2022-09-20 20:50:24 -0400

    LeetCode #263: Ugly Number

---
+++ 263/ugly_number.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+class Solution {
+public:
+    bool isUgly(int n) {
+        if (n < 1) {return false;}
+        
+        while (n > 1) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else if (n % 3 == 0) {
+                n /= 3;
+            } else if (n % 5 == 0) {
+                n /= 5;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main() {
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
