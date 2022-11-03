9b80b22950918b477cdff33dfd8e09f7584f14be
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2022-11-03 06:45:38 -0400

    LeetCode #50: Pow(x, n)

---
+++ 50/powx_n.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == 0 || n == 1){
+            return n == 0 ? 1 : x;
+        }
+        if (n == -1) {
+            return 1 / x;
+        }
+        
+        double power = myPow(x, n/2);
+        if (n % 2 == 0) {
+            return power * power;
+        } else {
+            return power * power * (n < 0 ? (1 / x) : x);
+        }
+    }
+};
+
+int main() {
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
