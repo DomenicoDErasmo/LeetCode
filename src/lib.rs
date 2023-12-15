460ce19bfbc8e8581f7e7f259c89dedec008604a
DomenicoDErasmo <44101761+DomenicoDErasmo@users.noreply.github.com>
2023-12-14 20:38:27 -0500

    p1688

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ pub mod p1512;
 pub mod p1641;
 pub mod p1646;
 pub mod p1672;
+pub mod p1688;
 pub mod p1920;
 pub mod p1929;
 pub mod p2011;
---
+++ src/p1688/mod.rs
@@ -0,0 +1,37 @@
+#![allow(dead_code)]
+
+struct Solution {}
+
+impl Solution {
+    pub fn number_of_matches(n: i32) -> i32 {
+        let casted_n = n as usize;
+        let mut answers = vec![0; casted_n + 1];
+        
+        for i in 2..=casted_n {
+            let even_subproblem = answers.get(i / 2).unwrap_or(&0).clone();
+            let odd_subproblem = answers.get((i-1) / 2 + 1).unwrap_or(&0).clone();
+
+            match answers.get_mut(i) {
+                Some(val) => {
+                    *val = match i % 2 == 0 {
+                        true => i / 2 + even_subproblem,
+                        false => (i - 1) / 2 + odd_subproblem,
+                    };
+                },
+                None => (),
+            };
+        }
+
+        *answers.get(casted_n).unwrap_or(&0) as i32
+    }
+}
+
+#[cfg(test)]
+mod p1688_tests {
+    use super::*;
+
+    #[test]
+    fn test_number_of_matches() {
+        assert_eq!(Solution::number_of_matches(7), 6);
+    }
+}
