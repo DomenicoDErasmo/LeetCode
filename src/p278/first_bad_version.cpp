561d930dafc479eb0f5ef47affdc7b6e24738fbd
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2025-01-30 21:44:46 -0500

    p278

--- src/p278/first_bad_version.cpp
+++ src/p278/first_bad_version.cpp
@@ -1,25 +1,32 @@
-#include <iostream>
-
-// Dummy stub so this compiles, although the test cases would probably implement it this way
-bool isBadVersion(int i) {
-    return i;
-}
-
-class Solution {
+class Solution
+{
 public:
-    int firstBadVersion(int n) {
-        int left = 0, right = n, mid = left + (right - left)/2;
-        while(left <= right) {
-            if (!isBadVersion(mid - 1) && isBadVersion(mid)) {return mid;}
-            else if (isBadVersion(mid)) {right = mid - 1;} 
-            else {left = mid + 1;}
-            mid = left + (right - left)/2;
+    int firstBadVersion(int n)
+    {
+        int low = 1, high = n;
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            if (isBadVersion(mid) && !isBadVersion(mid - 1))
+            {
+                return mid;
+            }
+            else if (isBadVersion(mid))
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
         }
-        return mid;
+        return -1;
+    }
+
+private:
+    /// stubbed to prevent error
+    bool isBadVersion(int version)
+    {
+        return true;
     }
 };
-
-int main() {
-    std::cout << "Hello world!" << std::endl;
-    return 0;
-}
