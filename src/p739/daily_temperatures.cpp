a17f6c12bbde21b54cf7a1b060f2cfa6792573bc
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2025-02-02 22:19:33 -0500

    p739

--- src/p739/daily_temperatures.cpp
+++ src/p739/daily_temperatures.cpp
@@ -3,12 +3,13 @@
 
 class Solution {
    public:
-    std::vector<int> dailyTemperatures(std::vector<int>& temperatures) {
+    std::vector<int> dailyTemperatures(std::vector<int> &temperatures) {
         std::vector<int> result(temperatures.size(), 0);
         std::stack<std::pair<int, int>> currentTemps;
         currentTemps.push({temperatures.front(), 0});
         for (int i = 1; i < temperatures.size(); i++) {
-            while (!currentTemps.empty() && temperatures[i] > currentTemps.top().first) {
+            while (!currentTemps.empty() &&
+                   temperatures[i] > currentTemps.top().first) {
                 auto top = currentTemps.top();
                 result[top.second] = i - top.second;
                 currentTemps.pop();
