a38c0b01154c438f8be68ab15387d67630d202c5
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2025-02-10 23:56:55 -0500

    p102

--- src/p102/binary_tree_level_order_traversal.cpp
+++ src/p102/binary_tree_level_order_traversal.cpp
@@ -1,55 +1,46 @@
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <vector>
 
 struct TreeNode {
     int val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode(): val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int in_val): val(in_val), left(nullptr), right(nullptr) {}
-    TreeNode(int in_val, TreeNode* in_left, TreeNode* in_right): val(in_val), left(in_left), right(in_right) {}
-    ~TreeNode() {
-        if (left) {delete left;}
-        if (right) {delete right;}
-    }
+    TreeNode *left, *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
 };
 
 class Solution {
-public:
-    struct LevelNode {
-        TreeNode* node;
-        size_t level;
-        LevelNode(TreeNode* node, size_t level): node(node), level(level) {}
-    };
-    
-    std::vector<std::vector<int>> levelOrder(TreeNode* root) {
-        if (!root) return {};
-        
+    std::vector<std::vector<int>> levelOrder(TreeNode *root) {
+        std::queue<std::pair<TreeNode *, int>> queue;
         std::vector<std::vector<int>> result;
-        std::queue<LevelNode> queue;
-        queue.push(LevelNode(root, 0));
+        if (!root) {
+            return result;
+        }
+        queue.push({root, 0});
+        result.push_back({});
+
         while (!queue.empty()) {
-            LevelNode level_node = queue.front();
-            if (level_node.node->left) {
-                queue.push(LevelNode(level_node.node->left, level_node.level + 1));
+            std::pair<TreeNode *, int> front = queue.front();
+            TreeNode *currentNode = front.first;
+            int currentLevel = front.second;
+
+            if (result.size() <= currentLevel) {
+                result.push_back({});
             }
-            if (level_node.node->right) {
-                queue.push(LevelNode(level_node.node->right, level_node.level + 1));
+
+            result[currentLevel].push_back(currentNode->val);
+            if (currentNode->left) {
+                queue.push({currentNode->left, currentLevel + 1});
             }
-            if (result.size() == level_node.level) {
-                result.push_back({level_node.node->val});
-            } else {
-                result.back().push_back(level_node.node->val);
+            if (currentNode->right) {
+                queue.push({currentNode->right, currentLevel + 1});
             }
+
             queue.pop();
         }
-        
+
         return result;
     }
 };
-
-int main() {
-    std::cout << "Hello world!" << std::endl;
-    return 0;
-}
