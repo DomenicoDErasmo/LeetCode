38dcbd819020eb6a0e3beb4b5b44a78bc12eba7f
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2025-02-14 00:11:36 -0500

    p39

--- src/p39/combination_sum.cpp
+++ src/p39/combination_sum.cpp
@@ -1,31 +1,38 @@
-#include <iostream>
 #include <vector>
 
 class Solution {
-public:
-    std::vector<std::vector<int>> combinationSum(std::vector<int>& candidates, int target) {
+   public:
+    std::vector<std::vector<int>> combinationSum(std::vector<int>& candidates,
+                                                 int target) {
         std::vector<std::vector<int>> result;
         std::vector<int> current;
         helper(candidates, target, 0, result, current);
         return result;
     }
-private:
-    void helper(const std::vector<int>& candidates, int target, int current_index, std::vector<std::vector<int>>& result, const std::vector<int>& current) {
+
+   private:
+    void helper(std::vector<int>& candidates, int target, int index,
+                std::vector<std::vector<int>>& result,
+                std::vector<int>& current) {
+        if (target < 0 || index == candidates.size()) {
+            return;
+        }
+
         if (target == 0) {
             result.push_back(current);
-        } else if (target > 0) {
-            for (size_t i = current_index; i < candidates.size(); i++) {
-                if (target - candidates[i] >= 0) {
-                    std::vector<int> new_current = current;
-                    new_current.push_back(candidates[i]);
-                    helper(candidates, target - candidates[i], i, result, new_current);
-                }
-            }
+            return;
         }
+
+        // skip
+        helper(candidates, target, index + 1, result, current);
+
+        // pick
+        target -= candidates[index];
+        current.push_back(candidates[index]);
+        helper(candidates, target, index, result, current);
+
+        // backtrack
+        target += candidates[index];
+        current.pop_back();
     }
 };
-
-int main() {
-    std::cout << "Hello world!" << std::endl;
-    return 0;
-}
