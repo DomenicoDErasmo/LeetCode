cb9dae9f3fc2adb0cae307b52b295204734ee547
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2025-03-03 22:13:16 -0500

    lint fix

--- src/p520/mod.rs
+++ src/p520/mod.rs
@@ -4,29 +4,30 @@ struct Config {
 }
 
 impl Config {
-    pub fn new(word: &String) -> Config {
+    pub fn new(word: &str) -> Config {
         let mut uppercases: [Option<bool>; 2] = [None, None];
-        for i in 0..2 {
-            let uppercase_mode = 
-                    if let Some(x) = word.chars().nth(i) {
+        for (i, item) in uppercases.iter_mut().enumerate() {
+            let uppercase_mode = if let Some(x) = word.chars().nth(i) {
                 x.is_uppercase()
             } else {
                 return Config {
                     other_letters_uppercase: None,
-                    early_return: Some(true)
+                    early_return: Some(true),
                 };
             };
-            uppercases[i] = Some(uppercase_mode);
+            *item = Some(uppercase_mode);
         }
 
-        let (other_letters_uppercase, 
-                early_return) = 
-                match (uppercases[0], uppercases[1]) {
-            (Some(false), Some(true)) => (None, Some(false)),
-            (_, _) => (uppercases[1], None),
-        };
+        let (other_letters_uppercase, early_return) =
+            match (uppercases[0], uppercases[1]) {
+                (Some(false), Some(true)) => (None, Some(false)),
+                (_, _) => (uppercases[1], None),
+            };
 
-        Config {other_letters_uppercase, early_return}   
+        Config {
+            other_letters_uppercase,
+            early_return,
+        }
     }
 }
 
@@ -36,7 +37,7 @@ impl DetectCapital {
     pub fn detect_capital_use(&self, word: String) -> bool {
         let config = Config::new(&word);
 
-        if config.early_return != None {
+        if config.early_return.is_some() {
             return config.early_return.unwrap();
         }
 
@@ -56,44 +57,43 @@ mod p520_tests {
 
     #[test]
     fn empty_string_true() {
-        let problem = DetectCapital{};
-        assert! (problem.detect_capital_use(String::new()));
+        let problem = DetectCapital {};
+        assert!(problem.detect_capital_use(String::new()));
     }
 
     #[test]
     fn one_char_true() {
-        let problem = DetectCapital{};
+        let problem = DetectCapital {};
         assert!(problem.detect_capital_use("a".to_string()));
     }
 
     #[test]
     fn first_lower_second_upper_false() {
-        let problem = DetectCapital{};
+        let problem = DetectCapital {};
         assert!(!problem.detect_capital_use("aBcd".to_string()));
     }
 
     #[test]
     fn all_uppercase_true() {
-        let problem = DetectCapital{};
+        let problem = DetectCapital {};
         assert!(problem.detect_capital_use("USA".to_string()));
     }
 
     #[test]
     fn all_lowercase_true() {
-        let problem = DetectCapital{};
+        let problem = DetectCapital {};
         assert!(problem.detect_capital_use("league".to_string()));
     }
 
     #[test]
     fn only_first_uppercase_true() {
-        let problem = DetectCapital{};
+        let problem = DetectCapital {};
         assert!(problem.detect_capital_use("Domenico".to_string()));
     }
 
     #[test]
     fn first_lowercase_others_uppercase_false() {
-        let problem = DetectCapital{};
+        let problem = DetectCapital {};
         assert!(!problem.detect_capital_use("aHa".to_string()));
     }
-
 }
