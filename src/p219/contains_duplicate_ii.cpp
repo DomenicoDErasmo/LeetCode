cb73792968b50e150c4b3fd362a9ca73b5775c70
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2025-03-06 22:42:40 -0500

    p219

--- src/p219/contains_duplicate_ii.cpp
+++ src/p219/contains_duplicate_ii.cpp
@@ -1,28 +1,23 @@
-#include <iostream>
-#include <vector>
-#include <set>
+#include <unordered_set>  // unordered_set
+#include <vector>         // vector
 
 class Solution {
-public:
+   public:
     bool containsNearbyDuplicate(std::vector<int>& nums, int k) {
-        if (k == 0) {return false;}
-        
-        std::set<int> k_interval;
-        for (size_t i = 0; i < nums.size(); i++) {
-            if (k_interval.find(nums[i]) != k_interval.end()) {
-                return true;
-            } else if (k_interval.size() < k) {
-                k_interval.insert(nums[i]);
-            } else {
-                k_interval.erase(nums[i - k]);
-                k_interval.insert(nums[i]);
+        std::unordered_set<int> valsInWindow;
+        int left = 0, right = 0;
+        for (int num : nums) {
+            if (right - left > k) {
+                valsInWindow.erase(nums[left]);
+                left++;
             }
+            
+            right++;
+            if (valsInWindow.find(num) != valsInWindow.end()) {
+                return true;
+            }
+            valsInWindow.insert(num);
         }
         return false;
     }
 };
-
-int main() {
-    std::cout << "Hello world!" << std::endl;
-    return 0;
-}
