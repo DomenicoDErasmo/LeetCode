ce078afd590eaf9000f3805b9de597710b5630b8
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2025-03-10 18:37:55 -0400

    p125

--- src/p125/valid_palindrome.cpp
+++ src/p125/valid_palindrome.cpp
@@ -1,39 +1,29 @@
-#include <iostream>
+#include <string>  // string
 
 class Solution {
-public:
+   public:
     bool isPalindrome(std::string s) {
-        parseInput(s);
-        if (s.length() <= 1) {return true;}
-        for (size_t i = 0; i <= s.length() / 2; i++) {
-            if (s[i] != s[s.length() - i - 1]) {return false;}
-        }
-        return true;
-    }
-private:
-    void makeLowercase(char& ch) {ch = char((int)ch + 32);}
-    
-    bool isLowercase(char ch) {return ('a' <= ch && ch <= 'z');}
-    
-    bool isNumber(char ch) {return '0' <= ch && ch <= '9';}
-    
-    bool isUppercase(char ch) {return ('A' <= ch && ch <= 'Z');}
-    
-    void parseInput(std::string& s) {
-        for (size_t i = 0; i < s.length(); i++) {
-            if (isLowercase(s[i]) || isNumber(s[i])) {
-                continue;
-            } else if (isUppercase(s[i])) {
-                makeLowercase(s[i]);
-            } else {
-                s.erase(i, 1);
-                i--;
+        std::string cleaned_string;
+        for (char ch : s) {
+            if (ch >= '0' && ch <= '9' || ch >= 'a' && ch <= 'z') {
+                cleaned_string.push_back(ch);
+            }
+
+            if (ch >= 'A' && ch <= 'Z') {
+                cleaned_string.push_back(ch + ' ');
             }
         }
+
+        int left = 0, right = cleaned_string.size() - 1;
+        while (left < right) {
+            if (cleaned_string[left] != cleaned_string[right]) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
     }
 };
-
-int main() {
-    std::cout << "Hello world!" << std::endl;
-    return 0;
-}
