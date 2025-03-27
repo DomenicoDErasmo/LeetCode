7a1d7fb0ca2e40ed08c55b7c41e69987aae46fc4
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2025-03-26 22:14:49 -0400

    p978

--- src/p978/longest_turbulent_subarray.cpp
+++ src/p978/longest_turbulent_subarray.cpp
@@ -1,41 +1,44 @@
-#include <vector>  // vector
+#include <vector>  // max, vector
 
 enum Comparison {
-    Equal = 0,
-    Less = 1,
+    Less = 0,
+    Equal = 1,
     Greater = 2,
 };
 
 Comparison compare(int left, int right) {
-    if (left == right) {
-        return Comparison::Equal;
-    } else if (left > right) {
-        return Comparison::Greater;
-    } else {
+    if (left < right) {
         return Comparison::Less;
+    } else if (left == right) {
+        return Comparison::Equal;
+    } else {
+        return Comparison::Greater;
+    }
+}
+
+int lengthFromComparison(Comparison current, Comparison previous,
+                         int currentLength) {
+    if (current == Comparison::Equal) {
+        return 1;
+    } else if (current == previous) {
+        return 2;
+    } else {
+        return currentLength + 1;
     }
 }
 
 class Solution {
    public:
     int maxTurbulenceSize(std::vector<int>& arr) {
-        if (arr.size() == 1) {
-            return 1;
-        }
-
-        Comparison lastComparison = compare(arr[0], arr[1]);
-
-        int maxLength = 0, curLength = 0;
+        int maxLength = 1, currentLength = 1;
+        Comparison previous = Comparison::Equal;
         for (int i = 1; i < arr.size(); i++) {
-            Comparison currentComparison = compare(arr[i - 1], arr[i]);
-            bool isTurbulent = (lastComparison != currentComparison &&
-                                lastComparison != Comparison::Equal &&
-                                currentComparison != Comparison::Equal);
-            lastComparison = currentComparison;
-            curLength = isTurbulent ? curLength + 1 : (currentComparison != Comparison::Equal ? 2 : 1);
-            maxLength = std::max(maxLength, curLength);
+            Comparison current = compare(arr[i - 1], arr[i]);
+            currentLength =
+                lengthFromComparison(current, previous, currentLength);
+            previous = current;
+            maxLength = std::max(maxLength, currentLength);
         }
-
         return maxLength;
     }
 };
