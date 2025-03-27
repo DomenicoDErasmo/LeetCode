ed1013082d5523b70b040271e61cba6e033aeea4
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2025-03-26 22:06:47 -0400

    rust linting

--- src/p1688/mod.rs
+++ src/p1688/mod.rs
@@ -6,20 +6,17 @@ impl Solution {
     pub fn number_of_matches(n: i32) -> i32 {
         let casted_n = n as usize;
         let mut answers = vec![0; casted_n + 1];
-        
-        for i in 2..=casted_n {
-            let even_subproblem = answers.get(i / 2).unwrap_or(&0).clone();
-            let odd_subproblem = answers.get((i-1) / 2 + 1).unwrap_or(&0).clone();
 
-            match answers.get_mut(i) {
-                Some(val) => {
-                    *val = match i % 2 == 0 {
-                        true => i / 2 + even_subproblem,
-                        false => (i - 1) / 2 + odd_subproblem,
-                    };
-                },
-                None => (),
-            };
+        for i in 2..=casted_n {
+            let even_subproblem = *answers.get(i / 2).unwrap_or(&0);
+            let odd_subproblem = *answers.get((i - 1) / 2 + 1).unwrap_or(&0);
+
+            if let Some(val) = answers.get_mut(i) {
+                *val = match i % 2 == 0 {
+                    true => i / 2 + even_subproblem,
+                    false => (i - 1) / 2 + odd_subproblem,
+                };
+            }
         }
 
         *answers.get(casted_n).unwrap_or(&0) as i32
