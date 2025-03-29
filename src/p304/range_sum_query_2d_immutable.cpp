8cc2ac98e84c12574823d953b2c13f7ae1e33f50
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2025-03-29 15:18:54 -0400

    p304

--- src/p304/range_sum_query_2d_immutable.cpp
+++ src/p304/range_sum_query_2d_immutable.cpp
@@ -3,35 +3,24 @@
 class NumMatrix {
    public:
     NumMatrix(std::vector<std::vector<int>>& matrix) {
-        std::vector<std::vector<int>> prefixSumToBuild(
-            matrix.size(), std::vector<int>(matrix.front().size(), 0));
-
-        // init first col
-        for (int i = 0; i < prefixSumToBuild.size(); i++) {
-            prefixSumToBuild[i][0] = matrix[i][0];
-        }
-
-        // fill each row
-        for (int i = 0; i < prefixSumToBuild.size(); i++) {
-            for (int j = 1; j < prefixSumToBuild[i].size(); j++) {
-                prefixSumToBuild[i][j] =
-                    prefixSumToBuild[i][j - 1] + matrix[i][j];
+        std::vector<std::vector<int>> initPrefixRangeSum(
+            matrix.size() + 1, std::vector<int>(matrix[0].size() + 1, 0));
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[i].size(); j++) {
+                initPrefixRangeSum[i + 1][j + 1] =
+                    initPrefixRangeSum[i][j + 1] +
+                    initPrefixRangeSum[i + 1][j] - initPrefixRangeSum[i][j] +
+                    matrix[i][j];
             }
         }
-
-        prefixSum = prefixSumToBuild;
+        prefixRangeSum = initPrefixRangeSum;
     }
 
     int sumRegion(int row1, int col1, int row2, int col2) {
-        int result = 0;
-        for (int i = row1; i <= row2; i++) {
-            result +=
-                prefixSum[i][col2] - (col1 == 0 ? 0 : prefixSum[i][col1 - 1]);
-        }
-
-        return result;
+        return prefixRangeSum[row2 + 1][col2 + 1] + prefixRangeSum[row1][col1] -
+               prefixRangeSum[row1][col2 + 1] - prefixRangeSum[row2 + 1][col1];
     }
 
    private:
-    std::vector<std::vector<int>> prefixSum;
+    std::vector<std::vector<int>> prefixRangeSum;
 };
