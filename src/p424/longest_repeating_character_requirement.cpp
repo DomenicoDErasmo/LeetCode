5e88813425ee3914397f41c263f4bb69d0f69611
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2025-03-29 01:11:33 -0400

    p424

--- src/p424/longest_repeating_character_requirement.cpp
+++ src/p424/longest_repeating_character_requirement.cpp
@@ -1,24 +1,37 @@
-#include <string>         // string
+#include <optional>       // optional
+#include <string>         // max, string
 #include <unordered_map>  // unordered_map
 
 class Solution {
    public:
     int characterReplacement(std::string s, int k) {
-        std::unordered_map<char, int> count;
-        int result = 0;
+        int left = 0;
+        std::optional<int> maxLength = std::nullopt,
+                           maxFrequency = std::nullopt;
+        std::unordered_map<char, int> frequencies;
 
-        int left = 0, maxFrequency = 0;
-        for (int right = 0; right < s.size(); right++) {
-            count[s[right]]++;
-            maxFrequency = std::max(maxFrequency, count[s[right]]);
+        for (int i = 0; i < s.length(); i++) {
+            frequencies[s[i]]++;
+            int currentFrequency = frequencies[s[i]];
+            maxFrequency =
+                maxFrequency.has_value()
+                    ? std::max(maxFrequency.value(), currentFrequency)
+                    : currentFrequency;
 
-            while ((right - left + 1) - maxFrequency > k) {
-                count[s[left]]--;
+            // only need to check distance between max frequency letter
+            // and string with all of that letter (i.e. size - max frequency)
+            while (distance(left, i) - maxFrequency.value() > k) {
+                frequencies[s[left]]--;
                 left++;
             }
-            result = std::max(result, right - left + 1);
+            maxLength = maxLength.has_value()
+                            ? std::max(maxLength.value(), distance(left, i))
+                            : distance(left, i);
         }
 
-        return result;
+        return maxLength.value_or(0);
     }
+
+   private:
+    int distance(int left, int right) { return right - left + 1; }
 };
