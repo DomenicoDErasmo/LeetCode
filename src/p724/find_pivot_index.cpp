a0ac9d48af8bbb3b2187a5386fc1aa7aec58cee1
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2025-03-29 15:48:19 -0400

    p724

--- src/p724/find_pivot_index.cpp
+++ src/p724/find_pivot_index.cpp
@@ -3,23 +3,18 @@
 class Solution {
    public:
     int pivotIndex(std::vector<int>& nums) {
-        std::vector<int> prefixSum(nums.size(), 0);
-        std::vector<int> postfixSum(nums.size(), 0);
-
+        int n = nums.size();
+        std::vector<int> prefixSum(n, 0), suffixSum(n, 0);
         for (int i = 1; i < nums.size(); i++) {
-            prefixSum[i] = prefixSum[i - 1] + nums[i-1];
+            prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
+            suffixSum[nums.size() - i - 1] =
+                suffixSum[nums.size() - i] + nums[nums.size() - i];
         }
-
-        for (int i = nums.size() - 2; i >= 0; i--) {
-            postfixSum[i] = postfixSum[i+1] + nums[i+1];
-        }
-
         for (int i = 0; i < nums.size(); i++) {
-            if (prefixSum[i] == postfixSum[i]) {
+            if (prefixSum[i] == suffixSum[i]) {
                 return i;
             }
         }
-
         return -1;
     }
 };
