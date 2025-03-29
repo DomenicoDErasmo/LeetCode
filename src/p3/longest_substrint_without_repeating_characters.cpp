3c94ed9a492d8c2ce5148038608ab4b361fe3a69
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2025-03-28 23:24:35 -0400

    p3

--- src/p3/longest_substrint_without_repeating_characters.cpp
+++ src/p3/longest_substrint_without_repeating_characters.cpp
@@ -1,31 +1,24 @@
-#include <string>         //string
-#include <unordered_set>  // unordered_set
+#include <optional>       // optional
+#include <string>         // string
+#include <unordered_set>  // min, unordered_set
 
 class Solution {
    public:
     int lengthOfLongestSubstring(std::string s) {
-        int right = 0;
         std::unordered_set<char> chars;
-        while (right < s.size() && chars.find(s[right]) == chars.end()) {
-            chars.insert(s[right]);
-            right++;
-        }
-
-        int left = 0, maxLength = right - left;
-
-        while (right < s.length()) {
-            char current = s[right];
-            if (chars.find(current) != chars.end()) {
-                while (chars.find(current) != chars.end()) {
-                    chars.erase(s[left]);
-                    left++;
-                }
+        int left = 0;
+        std::optional<int> maxLength = std::nullopt;
+        for (int i = 0; i < s.length(); i++) {
+            while (chars.count(s[i])) {
+                chars.erase(s[left]);
+                left++;
             }
-            chars.insert(current);
-            right++;
-            maxLength = std::max(maxLength, right - left);
+            chars.insert(s[i]);
+            int currentLength = i - left + 1;
+            maxLength = maxLength.has_value()
+                            ? std::max(currentLength, maxLength.value())
+                            : currentLength;
         }
-
-        return maxLength;
+        return maxLength.value_or(0);
     }
 };
